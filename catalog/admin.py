e9ebd46200c20c57a0237ab62c846e97978dc00f
joeblack24 <jvw8@georgetown.edu>
2022-04-29 19:06:29 -0400

    added to admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from models import Catalog
 
 # Register your models here.
+
+@admin.register(Catalog)
+class CatalogAdmin(admin.ModelAdmin):
+    list_display = ('object_id', 'data')
+    search_fields = ['object_id', 'data']
--- catalog/models.py
+++ catalog/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Catalog(models.Model):
     object_id = models.CharField(max_length=255, null=False, unique=True)
     data = models.JsonField()
+
+    class Meta:
+        db_table = 'catalog'
+        app_label = 'catalog'
